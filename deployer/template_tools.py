44467c4ab132dd96c986909df25d09aaeecaf4b9
Carl Waldbieser <cwaldbieser@gmail.com>
2017-08-22 17:07:01 -0400

    When a template has a syntax error, identify the file.

--- deployer/template_tools.py
+++ deployer/template_tools.py
@@ -1,8 +1,10 @@
 
 from __future__ import print_function
 import os
+import sys
 from fabric import utils as fabutils
 import jinja2
+from jinja2.exceptions import TemplateSyntaxError
 import yaml
 import deployer.config as config
 
@@ -23,7 +25,11 @@ def fill_templates():
     for fname, info in doc['files'].items():
         path = os.path.join(basedir, fname)
         with open(path) as f:
-            t = jinja2_env.from_string(f.read())
+            try:
+                t = jinja2_env.from_string(f.read())
+            except TemplateSyntaxError as ex:
+                fabutils.warn("Error processing template '{0}'.".format(path))
+                raise
             transformed = os.path.splitext(path)[0]
             with open(transformed, "w") as fout:
                 print(t.render(info['secrets']), file=fout)
