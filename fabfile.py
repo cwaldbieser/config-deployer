4892430ff1ec2b8fa80b06fd3eb7ed8da0131311
Carl Waldbieser <cwaldbieser@gmail.com>
2017-09-28 18:33:41 -0400

    Added Role introspection.

--- fabfile.py
+++ fabfile.py
@@ -3,6 +3,9 @@ from deployer import config
 from deployer.config_deployer import (
     deploy_config
 )
+from deployer.introspect import (
+    list_roles
+)
 from deployer.package_deployer import (
     install_package,
     install_local_package,
